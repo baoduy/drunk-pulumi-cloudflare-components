b75ed9036fb2030c1efb33916b21dd2f8b1d5bc5
Steven Hoang <drunkcoding@outlook.com>
2026-01-02 13:52:21 +0800

    up

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,18 +1,29 @@
 import * as pulumi from "@pulumi/pulumi";
 
+export const request = async (path:string, method:'GET'|'POST'|'PUT'|'DELETE', body?:any) => {
+    const url = `${process.env.CLOUDFLARE_API_BASE_PATH}/accounts/${process.env.CLOUDFLARE_ACCOUNT_ID}/${path}`;
+
+    const response = await fetch(url, {
+        method: method,
+        headers: {
+            Authorization: `Bearer ${process.env.CLOUDFLARE_API_TOKEN}`,
+            'Content-Type': 'application/json',
+        },
+        body: body ? JSON.stringify(body) : undefined,
+    });
+
+    if (!response.ok) {
+        const errorText = await response.text();
+        throw new Error(
+            `Error reading wrap app: ${response.status} ${response.statusText}\nError: ${errorText}`
+        );
+    }
+
+    return JSON.parse(await response.text()).result;
+}
+
 export async function getTunnelToken(accountId:string, tunnelId:string): Promise<string> {
-
-        const url = `https://api.cloudflare.com/client/v4/accounts/${accountId}/cfd_tunnel/${tunnelId}/token`;
-        const response = await fetch(url, {
-            method: 'GET',
-            headers: {
-                Authorization: `Bearer ${process.env.CLOUDFLARE_API_TOKEN}`,
-                'Content-Type': 'application/json',
-            },
-        });
-
-        return response.ok?JSON.parse(await response.text()).result:'';
-
+    return request(`cfd_tunnel/${tunnelId}/token`, 'GET');
 }
 
 export function getTunnelTokenOutput(accountId:pulumi.Input<string>, tunnelId:pulumi.Input<string>): pulumi.Output<string> {
--- src/zeroTrust/ZerotrustAccessWarp.ts
+++ src/zeroTrust/ZerotrustAccessWarp.ts
@@ -1,5 +1,6 @@
 import * as pulumi from '@pulumi/pulumi';
 import {BaseOptions, BaseProvider, BaseResource,} from '../base';
+import {request} from "../helpers";
 
 export interface ZeroTrustAccessWarpInputs {
     accountId: string;
@@ -11,47 +12,12 @@ export interface ZeroTrustAccessWarpInputs {
 export interface ZeroTrustAccessWarpOutputs extends ZeroTrustAccessWarpInputs {
 }
 
-const getWrap = async (accountId:string): Promise<any> => {
-    const url = `https://api.cloudflare.com/client/v4/accounts/${accountId}/access/warp`;
-
-    const response = await fetch(url, {
-        method: 'GET',
-        headers: {
-            Authorization: `Bearer ${process.env.CLOUDFLARE_API_TOKEN}`,
-            'Content-Type': 'application/json',
-        },
-    });
-
-    if (!response.ok) {
-        const errorText = await response.text();
-        throw new Error(
-            `Error reading wrap app: ${response.status} ${response.statusText}\nError: ${errorText}`
-        );
-    }
-
-    return JSON.parse(await response.text()).result;
+const getWrap = async (): Promise<any> => {
+    return request(`access/warp`,'GET');
 };
 
-const updateWrap = async (accountId:string,wrap:any): Promise<any> => {
-    const url = `https://api.cloudflare.com/client/v4/accounts/${accountId}/access/warp`;
-
-    const body = JSON.stringify(wrap);
-    const response = await fetch(url, {
-        method: 'PUT',
-        headers: {
-            Authorization: `Bearer ${process.env.CLOUDFLARE_API_TOKEN}`,
-        },
-        body,
-    });
-
-    if (!response.ok) {
-        const errorText = await response.text();
-        throw new Error(
-            `Error creating wrap app: ${response.status} ${response.statusText}\nError: ${errorText}\nBody: ${body}`
-        );
-    }
-
-    return JSON.parse(await response.text()).result;
+const updateWrap = async (wrap:any): Promise<any> => {
+    return request(`access/warp`,'PUT',wrap);
 };
 
 class ZeroTrustAccessWarpProvider extends BaseProvider<ZeroTrustAccessWarpInputs, ZeroTrustAccessWarpOutputs> {
@@ -60,7 +26,7 @@ class ZeroTrustAccessWarpProvider extends BaseProvider<ZeroTrustAccessWarpInputs
     }
 
     public async create(inputs: ZeroTrustAccessWarpInputs): Promise<pulumi.dynamic.CreateResult> {
-        const body = await getWrap(inputs.accountId);
+        const body = await getWrap();
         const newBody = {...body, policies: inputs.policies};
 
         if(!inputs.allowedIdps||inputs.allowedIdps.length===0){
@@ -69,7 +35,7 @@ class ZeroTrustAccessWarpProvider extends BaseProvider<ZeroTrustAccessWarpInputs
             newBody['allowed_idps']= inputs.allowedIdps;
             newBody['auto_redirect_to_identity']= inputs.allowedIdps.length===1?true:inputs.autoRedirectToIdp;
         }
-        await updateWrap(inputs.accountId,newBody);
+        await updateWrap(newBody);
         return {id:this.name, outs: inputs};
     }
 }
